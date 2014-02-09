4148b1f1f5866866432f53042c5cf00751085af9
bertrandbawbag <clairewright@iMac-C42C031C842A-9.local>
2014-02-09 11:34:33 +0000

    added anchor assembly class to repo

---
+++ Anchor Handler/AnchorAssembly.h
@@ -0,0 +1,28 @@
+//
+//  AnchorAssembly.h
+//  Spring
+//
+//  Created by Claire Wright on 21/01/2014.
+//  Copyright (c) 2014 Claire Wright. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+#import "Winch.h"
+#import "AnchorWire.h"
+#import "Anchor.h"
+
+
+
+@interface AnchorAssembly : SKNode
+
+@property (nonatomic, strong) Winch *winch;
+@property (nonatomic, strong) AnchorWire *wire;
+@property (nonatomic, strong) Anchor *anchor;
+
+
+-(id)initWithWinchPosition: (CGPoint) position;
+-(void)update:(CFTimeInterval)currentTime;
+-(void)didSimulatePhysics;
+-(void)didEvaluateActions;
+
+@end
---
+++ Anchor Handler/AnchorAssembly.m
@@ -0,0 +1,97 @@
+//
+//  AnchorAssembly.m
+//  Spring
+//
+//  Created by Claire Wright on 21/01/2014.
+//  Copyright (c) 2014 Claire Wright. All rights reserved.
+//
+
+#import "AnchorAssembly.h"
+
+
+@implementation AnchorAssembly
+
+BOOL _anchorMoved;
+BOOL _winchMoved;
+
+
+CGPoint _lastWinchPosition;
+CGPoint _lastAnchorPosition;
+
+
+
+#pragma mark - Initialisation and Set Up
+-(id)initWithWinchPosition: (CGPoint) position  {
+    
+    if (self = [super init]) {
+        
+        [self setUpWinch:position];
+        [self setUpAnchor];
+        [self setUpWire];
+        
+    }
+    
+    
+    return self;
+}
+
+
+-(void) setUpWinch: (CGPoint) position
+{
+    self.winch = [[Winch alloc] initWithColor:[SKColor greenColor] size:CGSizeMake(10, 10) position:position];
+    
+    
+    _lastWinchPosition = self.winch.position;
+    [self addChild:self.winch];
+}
+
+- (void) setUpAnchor
+{
+    self.anchor = [[Anchor alloc] initWithImageNamed:@"anchor"];
+    self.anchor.zPosition = self.winch.zPosition;
+    
+    _lastWinchPosition = self.anchor.position;
+    [self addChild:self.anchor];
+}
+
+-(void) setUpWire
+{
+    self.wire = [[AnchorWire alloc] initWithAnchor:self.anchor andWinch:self.winch];
+    self.wire.zPosition = self.anchor.zPosition - 1;
+    
+    [self addChild:self.wire];
+    
+    // testing only
+
+}
+
+-(void)update:(CFTimeInterval)currentTime
+{
+
+    
+}
+
+
+-(void)didEvaluateActions
+{
+    
+    [self.wire didEvaluateActions];
+    [self.winch didEvaluateActions];
+    [self.anchor didEvaluateActions];
+}
+
+-(void) didSimulatePhysics
+{
+
+    [self.wire didSimulatePhysics];
+    [self.winch didSimulatePhysics];
+}
+
+
+
+-(void) calculateForces
+{
+    
+}
+@end
+
