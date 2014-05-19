b33a5f966a7a1b9dd28feb9112877f98d856ecca
bertrandbawbag <clairewright@iMac-C42C031C842A-9.local>
2014-05-19 18:49:56 +0100

    added classes for button menu and buttons buttons taken from stack overflow http://stackoverflow.com/questions/20010331/correct-way-to-creat-button-in-sprite-kit?lq=1

---
+++ ANHButtonMenu.h
@@ -0,0 +1,13 @@
+//
+//  ANHButtonMenu.h
+//  Anchor Handler
+//
+//  Created by Claire Wright on 19/05/2014.
+//  Copyright (c) 2014 Brian. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface ANHButtonMenu : SKNode
+
+@end
